1604589fc975cb1ceaba270e217285c242fde571
lihaidehaoren <529509927@qq.com>
2022-11-30 22:01:49 +0800

    Update CourseDatabase.java

--- CourseDatabase.java
+++ CourseDatabase.java
@@ -1,11 +1,68 @@
 import java.util.ArrayList;
 
 public class CourseDatabase {
+
     private ArrayList<Course> courseList;
 
-    //Default Constructor and Constructor that you think it's necessary.
-    public CourseDatabase(){}
+    public CourseDatabase(){
+        ArrayList<Course> courses=new ArrayList<>();
+        courseList=courses;
+    }
 
-    public void addCourse(Course newCourse);
-    public void removeCourse(String courseID, int classID);
+
+
+    public void addCourse(Course newCourse){
+        courseList.add(newCourse);
+    }
+    public void removeCourse(String courseID, int classID){
+        for (Course c:courseList){
+            if (c.getCourseID().equals(courseID)&&c.getClassID()==classID){
+                courseList.remove(c);
+            }
+        }
+    }
+
+    public ArrayList<Course> searchCourseByID(String courseID){
+        ArrayList<Course> result=new ArrayList<>();
+        for (Course c:courseList){
+            if (c.getCourseID().equals(courseID)){
+                result.add(c);
+            }
+        }
+        return result;
+    }
+
+    public ArrayList<Course> searchCourseByTeacherName(String teacherName){
+        ArrayList<Course> result=new ArrayList<>();
+        for (Course c:courseList){
+            if (c.getTeacherName().equals(teacherName)){
+                result.add(c);
+            }
+        }
+        return result;
+    }
+
+    public ArrayList<Course> searchCourseByCourseName(String courseName){
+        ArrayList<Course> result=new ArrayList<>();
+        for (Course c:courseList){
+            if (c.getCourseName().equals(courseName)){
+                result.add(c);
+            }
+        }
+        return result;
+    }
+
+    public ArrayList<Course> searchCourseByCourseTime(String startTime, String endTime){
+        ArrayList<Course> result=new ArrayList<>();
+        for (Course c:courseList){
+            if (c.getTimeList().get(0).getEndTime().equals(endTime)&&c.getTimeList().get(0).getStartTime().equals(startTime)||c.getTimeList().get(1).getEndTime().equals(endTime)&&c.getTimeList().get(1).getStartTime().equals(startTime)){
+                result.add(c);
+            }
+        }
+        return result;
+    }
+
+    public ArrayList<Course> getCourseList(){
+        return courseList;
+    }
 }
